17d2a67d3cefa7820f6af9f586f2463d889d6147
RobertoMMM <91472462+RobertoMMM@users.noreply.github.com>
2022-05-24 11:48:15 +0300

    main

--- src/weather/allData/current data/CurrentWeather.jsx
+++ src/weather/allData/current data/CurrentWeather.jsx
@@ -33,7 +33,7 @@ const CurrentWeather = () => {
           </div>
         </div>
         <div className="localWeatherData">
-          <img src={response} className="imgCondition" />
+          <img src={response} className="imgCondition" alt="img"/>
           <div className="localTemp">
             {allData.current.temp_c}
             <RiCelsiusFill />
